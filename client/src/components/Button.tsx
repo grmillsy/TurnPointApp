9749f797fe0406721c881844352031c3253a4103
grmillsy <georgia.r.mills@hotmail.co.uk>
2024-09-18 14:58:02 +1000

    refactored icons and re-added button disabled css

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,8 +1,8 @@
 import {ButtonProps} from "../utils/types.ts";
 
-const Button = ({children, type, onClick, disabled}: ButtonProps) => (
+const Button = ({children, type, onClick, disabled}: ButtonProps) =>  (
         <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="disabled:bg-slate-400 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type={type ? type : "button"}
             onClick={onClick}
             disabled={disabled}
